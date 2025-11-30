a5b27b7c0682654c72fc9046b77b575cadd8aab6
Rj Akash Chowdury <hafejmohammadakash@gmail.com>
2025-11-30 20:02:50 +0600

    data update  er kaj korlam

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -14,26 +14,40 @@ router.get("/:id", async (req, res) => {});
 router.post("/", async (req, res) => {
   try {
     const todo = new Todo(req.body);
-    const savedTodo = await todo.save();  
+    const savedTodo = await todo.save();
     res.status(201).json(savedTodo);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 });
 
-
 // post the todo
 router.post("/all", async (req, res) => {
-    try {
-        const multiple = await Todo.insertMany(req.body);
-        res.status(201).json(multiple);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  try {
+    const multiple = await Todo.insertMany(req.body);
+    res.status(201).json(multiple);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 //  post multiple the todo
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedTodo = await Todo.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          status: "active",
+          title: "jhinuk",
+        },
+      }
+    );
+    res.status(200).json(updatedTodo);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 // delete the todo
 router.delete("/:id", async (req, res) => {});
