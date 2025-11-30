171a19d35896d40e0b5509fa47cbb70a5d85cf2b
Rj Akash Chowdury <hafejmohammadakash@gmail.com>
2025-11-30 17:54:20 +0600

    step 1 korlam

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const  express  = require("express");
+const mongoose = require("mongoose");
 
+// express app initialization
+const app = express();
+app.use(express.json())
+
+// database connection with mongoose
+
+
+
+// application route
+
+
+
+// error function handler
+function errorHandler(error, req, res, next) {
+    if(res.headersSent){
+        return (error)
+    }
+    res.status(500).json({error: error})
+}
+
+app.listen(3000, () => {
+    console.log("app listening at the port 300");
+})
 
